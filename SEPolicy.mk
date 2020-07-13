ba8572c217d35626a4310fefaaf8506594264c31
Michael Bestas <mkbestas@lineageos.org>
2020-07-13 19:40:01 +0300

    bouquet: Remove ifaa/soter/mlipay support
    Change-Id: I93d0e3848d1d0119257c3ab2e8e8d11a6b753f00
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -6,5 +6,3 @@
 
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/vendor/xiaomi
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/public
