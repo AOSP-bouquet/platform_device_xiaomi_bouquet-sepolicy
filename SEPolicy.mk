90f12776d2b033783cc38d7cbfa13ed38b6351ad
Reinazhard <reinazhard@gmail.com>
2021-11-26 18:15:36 +0100

    bouquet: Initial import from device tree
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -3,3 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/vendor/xiaomi
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/private
