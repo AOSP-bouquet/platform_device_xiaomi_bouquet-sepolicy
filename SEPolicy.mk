9f8c54ed7b17c503cd4c616384fa9e1b03d35fe9
Reinazhard <reinazhard@gmail.com>
2022-04-03 18:41:26 +0200

    bouquet: Inital sepolicy import
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -3,3 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/vendor/xiaomi
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/xiaomi/bouquet-sepolicy/public
